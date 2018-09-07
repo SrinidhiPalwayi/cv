756893cd10f35ea698d85934eb1aea91f2fe2ad6
SrinidhiPalwayi <srinidhipalwayi@lawn-143-215-113-245.lawn.gatech.edu>
2018-09-07 16:22:37 -0400

    new

--- computeH.py
+++ computeH.py
@@ -1,14 +1,15 @@
 import matplotlib.pylab as plt
 import numpy as np
+import sys
 
 """"
 run script twice
 change the list to the other image and another text file
 """
-
 """""
-right_file = "right.png"
-left_file = "left.png"
+
+right_file = "crop1.png"
+left_file = "crop2.png"
 r_img = plt.imread(right_file)
 l_img = plt.imread(left_file)
 f,a = plt.subplots(1,2, figsize=(15,10))
@@ -29,13 +30,18 @@ print(pos_right)
 """""
 import json
 
-orig_img_size = 2160
 new_img_size = 2.0
-scaler = lambda t: t * (new_img_size / float(orig_img_size))
+scaler = lambda t,orig_img_size : t * (new_img_size / float(orig_img_size))
+
+
 
 def computeH():
-    left_list = getScaledPoints("left.txt")
-    right_list =  getScaledPoints("right.txt")
+    left_list = np.load('cc1.npy')
+        #getScaledPoints("left.txt")
+    right_list =  np.load('cc2.npy')
+
+    #why is the identity matrix diagonal half
+        #getScaledPoints("right.txt")
 
     n = len(left_list)
 
@@ -69,7 +75,6 @@ def computeH():
 
     u, s, vh = np.linalg.svd(A)
     H = vh[-1].reshape(3,3)
-
     return H
 
 def getScaledPoints(textfile):
@@ -84,17 +89,86 @@ def warpImage(inputIm, refIm, H):
     #inputIm is left image
     #refIm is the right image
     input = np.asarray(plt.imread(inputIm))
-    width, height, channels = input.shape
+    print(input)
+    height, width, channels = input.shape
+    print(height, width, channels)
 
-    A =[[scaler(w), scaler(h), 1] for h in range(height) for w in range(width)]
+    top_left = np.asarray([0,0,1]).reshape(3,1)
+    top_right = np.asarray([width, 0, 1]).reshape(3,1)
+
+    bot_left = np.asarray([0,height, 1]).reshape(3,1)
+    bot_right = np.asarray([width, height, 1]).reshape(3,1)
+
+    corners = np.concatenate((top_left, top_right, bot_left, bot_right), axis=1)
+    forward_warped = np.dot(H, corners)
+
+    x_max=y_max = -sys.maxint
+    x_min=y_min = sys.maxint
+
+    scale_warped = list()
+    for col in forward_warped.T:
+        row = np.true_divide(col, col[2])
+        if (row[0] > x_max):
+            x_max = row[0]
+        elif (row[0] < x_min):
+            x_min = row[0]
+        if (row[1] > y_max):
+            y_max = row[1]
+        if (row[1] < y_min):
+            y_min = row[1]
+        scale_warped.append(row)
+    scale_warped = np.asarray(scale_warped)
+    scale_warped = np.transpose(scale_warped)
+    print(scale_warped)
+    A = np.asarray([[w, h, 1] for h in range(int(np.floor(y_min)), int(np.ceil(y_max)))
+                    for w in range(int(np.floor(x_min)),int(np.ceil(x_max)))])
+    A = np.transpose(A)
+    H_inv = np.linalg.inv(H)
+
+    inverse_warp = np.dot(H_inv, A)
+    scale_inverse_warp = list()
+    for col in inverse_warp:
+        row = np.true_divide(col, col[2])
+        scale_inverse_warp.append(row)
+    scale_inverse_warp = np.asarray(scale_inverse_warp)
+    scale_inverse_warp = np.transpose
+
+
+
+
+""""
     transformed = np.dot(H, np.transpose(A))
-    print(len(transformed), len(transformed[0]))
-    print(transformed)
 
 
 
+    scale_w = list()
+    x_max = -sys.maxint
+    x_max_y = 0
+    x_min = sys.maxint
+    x_min_y = 0
 
-
+    y_max = -sys.maxint
+    y_max_x = 0
+    y_min = sys.maxint
+    y_min_x =0
+    for col in transformed.T:
+        row = np.true_divide(col, col[2])
+        if(row[0] > x_max):
+            x_max = row[0]
+            x_max_y = row[1]
+        elif(row[0] < x_min):
+            x_min = row[0]
+            x_min_y = row[1]
+        if(row[1] > y_max):
+            y_max_x = row[0]
+            y_max = row[1]
+        if(row[1] < y_min):
+            y_min_x = row[0]
+            y_min = row[1]
+        scale_w.append(row)
+    print(len(scale_w), len(scale_w[0]))
+    print(x_min, x_min_y, x_max, x_max_y, y_min, y_min_x, y_max, y_max_x)
+"""
 
 
 #one_list= np.ones((1,n), dtype=int)
@@ -103,8 +177,10 @@ def warpImage(inputIm, refIm, H):
 #transformed = np.dot(H, left_side)
 
 #print (vh.shape)
+#print (vh.shape)
 #print(A.shape)
 #print(H)
 
 #print(right_list)
-print(warpImage("left.png", "right.png", computeH()))
+#print(computeH())
+print(warpImage("crop1.jpg", "crop2.jpg", computeH()))
